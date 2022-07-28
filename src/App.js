dbbcf83dcf4bb4cdf0bdfa980ee4846eb6257851
Hector Agudelo <haacny86@gmail.com>
2022-07-28 18:59:32 -0400

    created Img component for the navbar logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Search} from './components/Search'
+import {Logo} from './components/NavBarStyles'
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+<Logo/>
       <Search/> 
     </div>
   );
---
+++ src/components/NavBarStyles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import logoImg from "./../../Resourses/logo.png"
+
+
+export const Logo = styled.img`
+background-image: url(${logoImg});
+`
