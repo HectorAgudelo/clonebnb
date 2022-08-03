448c4dbc8d0f9b3a46a9f322225e961790abbe00
Hector Agudelo <haacny86@gmail.com>
2022-08-03 16:25:54 -0400

    created input container element

--- src/components/SearchNav.js
+++ src/components/SearchNav.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import {
-    ExpandSearchContainer as ExpandSearchContainer
+    ExpandSearchContainer,
+   inputContainer
 } from './SearchNavStyle'
 
 
 export const SearchNav = ({closeInput}) =>{
     return (
         <ExpandSearchContainer onClick={()=>closeInput(false)}>
-            Expanded Navigation Bar
-           
+            
+           <inputContainer></inputContainer>
         </ExpandSearchContainer>
         
     )
--- src/components/SearchNavStyle.js
+++ src/components/SearchNavStyle.js
@@ -1,10 +1,23 @@
 import styled from "styled-components";
 
-export const ExpandSearchContainer = styled.div`
+export const ExpandSearchContainer = styled.section`
 position: absolute;
-    left: 0px;
-    top: 0px;
-width: 100%;
-height: 50%;
-background-color: red;
+width: 1440px;
+height: 460px;
+left: 0px;
+top: 0px;
+
+background: #FFFFFF;
+`
+
+export const inputContainer = styled.section`
+position: absolute;
+width: 1248px;
+height: 55px;
+left: 96px;
+top: 93px;
+
+background: #FFFFFF;
+box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
+border-radius: 16px;
 `
