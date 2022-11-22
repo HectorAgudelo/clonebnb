ca261fc5a38a9d40aead05872dc91eff6cac4a01
Hector Agudelo <haacny86@gmail.com>
2022-11-22 09:03:04 -0500

    feat: adding modal effect to the search form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import logo from '../Resources/logo.png';
-import { BsSearch } from 'react-icons/bs';
 import { FaSearch } from 'react-icons/fa';
 
 export const NavBar = () => {
-//   const [navOpen, setNavOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
+  const outsideClick = useRef(null)
+  
+console.log(outsideClick.current)
+  useEffect(()=>{
+    document.addEventListener("click",handleClickOutside, true)
+  })
+
+  const handleClickOutside=()=>{
+    if(!outsideClick){
+      setNavOpen(true)
+    } else {
+      setNavOpen(false)
+    }
+  }
+
   return (
     <div className='flex flex-row justify-between py-4 px-16 items-center'>
-      <img className='' src={logo} alt='logo' />
-      <div className='flex flex-row justify-center items-center cursor-pointer' >
-        <div className=' border-zinc-200 border-y-2 border-l-2 rounded-l-xl p-3'>
+      <img className={`${navOpen?'hidden': 'static'}`} src={logo} alt='logo' />
+      <div className={`flex flex-row justify-center items-center cursor-pointer ${navOpen?'hidden': 'static'}`} onClick={()=>setNavOpen(true)} >
+        <div className=' border-zinc-200 border-y-2 border-l-2 rounded-l-2xl p-3'>
           <p>Helsinki, Finland</p>
         </div>
         <div className=' border-y-zinc-200 border-2  p-3'>
           <p>Add Guest</p>
         </div>
-        <div className=' border-x-zinc-200 border-y-2 border-r-2 rounded-r-xl p-4'>
+        <div className=' border-x-zinc-200 border-y-2 border-r-2 rounded-r-2xl p-4'>
           <FaSearch className='text-red-400' />
         </div>
       </div>
+     {navOpen ? <div ref={outsideClick}>
+        Hello
+      </div>
+      : null}
     </div>
   );
 };
